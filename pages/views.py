669a109debf60e6449676c29c542c6048056d944
Hispida <coryphella@mail.ru>
2022-03-25 01:58:53 +0300

    colored headline, "publication list"

--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,37 @@
 from django.views.generic import TemplateView
 import requests
 import json
-from MyPublications import publications_list
 
+
+def publications_list():
+    my_api_token = "Token 1c78af92d5738971f4f5b45b8e315d4bd8da41bd"
+
+    publications = "https://publons.com/api/v2/academic/publication/?academic=J-5196-2018"
+
+    header = {"Authorization": my_api_token}
+    response = requests.get(publications, headers=header)
+    publications_data = response.json()
+    results = publications_data["results"]
+    publication_list = []
+    number = 0
+    for i in results:
+        # for l in results:
+        journal = (i["journal"]["name"])
+        title = (i["publication"]["title"])
+        date = (i["publication"]["date_published"])[0:4]  # слайс от строки с датой
+        # year = (i)
+        number += 1
+        publication_list.append(f"{number}).{title}, {journal}, {date}\n")
+
+    return publication_list
 
 class HomePageView(TemplateView):
     template_name = "home.html"
     text = "MASH"
 
+
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-
-        pub_list = publications_list()
-        print(pub_list)
-        context.update({'some_list': pub_list['results']})
+        context.update({'pub_list': publications_list()})
         return context
 # Create your views here.
